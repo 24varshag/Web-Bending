58ca188d892e93010bea68a338d2d418f6a507dc
C0D3GH0ST <167582022+C0D3GH0ST@users.noreply.github.com>
2025-04-28 00:26:27 -0400

    Styling additions + Katara in, Azula out

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let selectedCharacters = new Set();
 let characterData = {};
 
 window.onload = async () => {
-  on(); // Show overlay
-  setTimeout(slideDoors, 500); // Slide doors after a delay
+  //on(); // Show overlay
+  //setTimeout(slideDoors, 500); // Slide doors after a delay
   characterData = await d3.json('character_top_words.json');
   initializeCharacterButtons();
   createWordCloud('top_global_words.json', '#wordcloud');
